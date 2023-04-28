8c57f8b5b45c68ef7572b3c60cf90aea58d12e01
Fazle Bari <fazlebarisn@gmail.com>
2023-04-28 12:12:07 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import "./style.css"
+
 export default function App(){
-  return "Hy"
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text"  id="item"/>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo list</h1>
+      <ul className="list">
+        <li>
+          <label htmlFor="">
+            <input type="checkbox"/>
+            item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  )
 }
