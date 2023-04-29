d6cea17b504184680c6447318545019a79ef80c9
Fazle Bari <fazlebarisn@gmail.com>
2023-04-29 11:10:53 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,25 @@ export default function App(){
         { id: crypto.randomUUID(), title: newItem, complated: false},
       ]
     })
+    // empty inputbox
+    setNewItem('')
+  }
+
+  function toggleTodo(id, complated){
+    setTodos( currentTodos => {
+      return currentTodos.map(todo => {
+        if( todo.id === id ){
+          return { ...todo, complated }
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos( currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
   }
 
   return (
@@ -33,14 +52,23 @@ export default function App(){
       </form>
       <h1 className="header">Todo list</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         { todos.map( todo => {
           return (
             <li key={todo.id}>
             <label>
-              <input type="checkbox" checked={todo.complated} />
+              <input 
+                type="checkbox" 
+                checked={todo.complated} 
+                onChange={ e=> toggleTodo(todo.id, e.target.checked) }
+              />
               {todo.title}
             </label>
-            <button className="btn btn-danger">Delete</button>
+            <button
+              onClick={() => deleteTodo(todo.id)} 
+              className="btn btn-danger">
+              Delete
+            </button>
           </li>
           )
         })}
